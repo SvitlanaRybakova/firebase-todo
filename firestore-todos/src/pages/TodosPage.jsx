14705ed59b55c2d6aca8fbb66fddaa0b4055fc54
Svitlana <devop.edu@gmail.com>
2021-12-02 15:48:38 +0100

    connected firebase

--- firestore-todos/src/pages/TodosPage.jsx
+++ firestore-todos/src/pages/TodosPage.jsx
@@ -1,33 +1,46 @@
-import React from 'react'
-import { Button, Container, ListGroup } from 'react-bootstrap'
+import React, { useEffect, useState } from "react";
+import { Button, Container, ListGroup } from "react-bootstrap";
+import { db } from "../firebase";
+import { collection, getDocs } from "firebase/firestore";
 
-const todos = [
-	{
-		id: '14c9b3244b4a',
-		title: 'Learn React 😊',
-		completed: true,
-	},
-	{
-		id: '5e584050fc4f',
-		title: 'Learn Firebase 🔥',
-		completed: false,
-	},
-	{
-		id: 'd3329c34dc67',
-		title: 'Profit 💰',
-		completed: false,
-	},
-	{
-		id: '44fd9cc7e1a4',
-		title: 'Take over the world 😈',
-		completed: false,
-	}
-]
+// const todos = [
+// 	{
+// 		id: '14c9b3244b4a',
+// 		title: 'Learn React 😊',
+// 		completed: true,
+// 	},
+// 	{
+// 		id: '5e584050fc4f',
+// 		title: 'Learn Firebase 🔥',
+// 		completed: false,
+// 	},
+// 	{
+// 		id: 'd3329c34dc67',
+// 		title: 'Profit 💰',
+// 		completed: false,
+// 	},
+// 	{
+// 		id: '44fd9cc7e1a4',
+// 		title: 'Take over the world 😈',
+// 		completed: false,
+// 	}
+// ]
 
 const TodosPage = () => {
+	const [todos, setTodos] = useState([]);
+	const [setLoading, setLoadind] = useState(true);
+
+	useEffect(async() => {
+		// get collection reference
+		const colRef = collection(db, 'todos')
+		// create snabshot
+		const snabshot = await getDocs(colRef);
+	
+	}, []);
+
+
 	return (
 		<Container className="py-3">
-
 			<div className="d-flex justify-content-between align-items-center mb-3">
 				<h1>Todos</h1>
 				<Button onClick={() => {}}>Refresh</Button>
@@ -35,14 +48,17 @@ const TodosPage = () => {
 
 			<ListGroup>
 				{todos.map((todo, index) => (
-					<ListGroup.Item action href={`/todos/${todo.id}`} key={index}>
+					<ListGroup.Item
+						action
+						href={`/todos/${todo.id}`}
+						key={index}
+					>
 						{todo.title}
 					</ListGroup.Item>
 				))}
 			</ListGroup>
-
 		</Container>
-	)
-}
+	);
+};
 
-export default TodosPage
+export default TodosPage;
