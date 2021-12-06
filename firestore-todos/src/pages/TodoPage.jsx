649c5a4cab7fc9e7563a4c459930f8b04da8261e
Svitlana <devop.edu@gmail.com>
2021-12-06 12:04:28 +0100

    implemented the deleteTodo func

--- firestore-todos/src/pages/TodoPage.jsx
+++ firestore-todos/src/pages/TodoPage.jsx
@@ -1,23 +1,30 @@
 import React from "react";
 import { Button, ButtonGroup, Container } from "react-bootstrap";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import useGetTodo from "../hooks/useGetTodo";
 import { db } from "../firebase";
-import { doc, updateDoc } from "firebase/firestore";
+import { doc, updateDoc, deleteDoc } from "firebase/firestore";
 
 const TodoPage = () => {
 	const { id } = useParams();
 	const { getTodo, loading, data } = useGetTodo(id);
+	const navigate = useNavigate();
 
 	const toggleTodo = async () => {
 		const ref = doc(db, "todos", id);
 		await updateDoc(ref, {
 			completed: !data.completed,
 		});
-		getTodo()
+		getTodo();
 	};
 
-	
+	const deleteTodo = async () => {
+		const ref = doc(db, "todos", id);
+		await deleteDoc(ref);
+		// redirect user to todos list
+		navigate("/todos", { replace: true });
+	};
+
 	return (
 		<Container className="py-3">
 			{loading && <p>Loading...</p>}
@@ -26,17 +33,14 @@ const TodoPage = () => {
 					<div className="d-flex justify-content-between align-items-center mb-3">
 						<h1>{data.title}</h1>
 
-						<Button
-							onClick={getTodo}>
-							Refresh
-						</Button>
+						<Button onClick={getTodo}>Refresh</Button>
 					</div>
 					<p className="display-1">{data.completed ? "🥳" : "🥵"}</p>
 					<ButtonGroup className="todo-actions">
 						<Button variant="primary" onClick={toggleTodo}>
 							Toggle
 						</Button>
-						<Button variant="danger" onClick={() => {}}>
+						<Button variant="danger" onClick={deleteTodo}>
 							Delete
 						</Button>
 					</ButtonGroup>
